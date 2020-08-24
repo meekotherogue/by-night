79a0ccfc5954ac8aa02563c019e1b26dde694c83
Martina Nagy <martina@7shifts.com>
2020-08-24 16:39:08 -0600

    Formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
-const fg = require('fast-glob');
+const fg = require("fast-glob");
 
-const imgs = fg.sync(['**/img/*', '!**/_site']);
+const imgs = fg.sync(["**/img/*", "!**/_site"]);
 
-module.exports = (function(eleventyConfig) {
-  eleventyConfig.addCollection('imgs', function(collection) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addCollection("imgs", function (collection) {
     return imgs;
   });
-});
+};
