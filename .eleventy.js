3cb751cc653907de8e0bfe4ce484669b7a2f9716
Martina Nagy <martina@7shifts.com>
2020-08-24 16:40:07 -0600

    Fixing issue with rogue images

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const fg = require("fast-glob");
 
-const imgs = fg.sync(["**/img/*", "!**/_site"]);
+const imgs = fg.sync(["./img/*", "!**/_site"]);
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("imgs", function (collection) {
