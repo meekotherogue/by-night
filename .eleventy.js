897cea40c17d879aa7249622c724ca4e65e13515
Martina Nagy <martina@7shifts.com>
2020-08-30 17:14:25 -0600

    Removing carousel for now

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 const fg = require("fast-glob");
-const ampPlugin = require("@ampproject/eleventy-plugin-amp");
+// const ampPlugin = require("@ampproject/eleventy-plugin-amp");
 
 const art2017 = fg.sync(["./img/art/2017/*", "!**/_site"]);
 const art2018 = fg.sync(["./img/art/2018/*", "!**/_site"]);
 
 module.exports = function (eleventyConfig) {
   // Plugins
-  eleventyConfig.addPlugin(ampPlugin);
+  // eleventyConfig.addPlugin(ampPlugin);
 
   // Collections
   eleventyConfig.addCollection("art2017", function (collection) {
