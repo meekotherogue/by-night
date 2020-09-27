c8876da8cc08ca0105160d86cabf97ca5745fdc3
Martina Nagy <martina@7shifts.com>
2020-09-27 10:14:39 -0600

    WIP using eleventy-img to process images

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,39 @@ const fs = require("fs");
 const fg = require("fast-glob");
 const imagesResponsiver = require("eleventy-plugin-images-responsiver");
 
+const Image = require("@11ty/eleventy-img");
+function eleventyImage(className, type, year, fileName, widths) {
+  var outputFormat = "jpg";
+  // returns Promise
+  var stats = Image(`${fileName}`, {
+    widths: widths,
+    formats: [outputFormat],
+    urlPath: `img/${type}/${year}`,
+    outputDir: `_site/dist/img/${type}/${year}`,
+  });
+  let props = stats[outputFormat].pop();
+
+  return `<img src="${props.url}"
+          width="${props.width}"
+          height="${props.height}"
+          alt="${fileName}"
+          class="${className}">`;
+}
+
 module.exports = function (eleventyConfig) {
   // Plugins
-  eleventyConfig.addPlugin(imagesResponsiver);
+  // eleventyConfig.addPlugin(imagesResponsiver);
+
+  // Javascript
+  eleventyConfig.addLiquidShortcode("galleryThumbnailImage", function (
+    type,
+    year,
+    fileName
+  ) {
+    return eleventyImage("gallery__thumbnailimage", type, year, fileName, [
+      300,
+    ]);
+  });
 
   // Passthrough Copy
   eleventyConfig.addPassthroughCopy("css");
