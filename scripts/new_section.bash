85556157b10ed0725436c0cfb3eeca2dfe89a4a8
Martina Rauert <martinarauert@gmail.com>
2023-01-16 14:53:55 -0600

    Adding new files

--- scripts/new_section.bash
+++ scripts/new_section.bash
@@ -71,18 +71,41 @@ meta_out="./_data/${type}Meta${subtype}.json"
 echo "sh ./scripts/generate_meta_json.bash ${subtype_out_dir} \"${desc}\" ${subtype} ${meta_out}\n"
 sh ./scripts/generate_meta_json.bash ${subtype_out_dir} "${desc}" ${subtype} ${meta_out}
 
-# for file in $dir*
-# do
-#   echo $file
-#   echo "curl --user api:${TINYPNG_API} --data-binary @$file -i https://api.tinify.com/shrink | jq '.output.url' | tr -d '\"'"
-#   url=$(curl --user api:${TINYPNG_API} --data-binary @$file -s https://api.tinify.com/shrink | jq ".output.url" | tr -d '"')
-#   echo "curl $url --output $file"
-#   downloadResult=$(curl $url --output $file)
-# done
+old_type="art"
+old_subtype="2017"
+echo "\nCreating templates from ${old_type} and ${old_subtype}...\n"
 
-# dir=$1
-# desc=$2
-# year=$3
-# out=$4
+new_data_file="_data/${type}${subtype}.js"
+echo "Creating ${new_data_file}"
+cp _data/${old_type}$old_subtype.js ${new_data_file}
+sed -i.bak "s/${old_type}/${type}/g" ${new_data_file}
+sed -i.bak "s/${old_subtype}/${subtype}/g" ${new_data_file}
 
-# sh ./scripts/optimize_images.bash ./img/commissions/2022/
+echo "Creating markdown..."
+if [ "$is_new_type" = 1 ]
+then
+  mkdir "$type"
+  new_base_markdown_file="${type}/${type}.md"
+  cp "${old_type}/${old_type}.md" ${type}
+  sed -i.bak "s/${old_type}/${type}/g" ${new_base_markdown_file}
+  sed -i.bak "s/${old_subtype}/${subtype}/g" ${new_base_markdown_file}
+fi
+new_markdown_dir="$type/$subtype"
+mkdir "$new_markdown_dir"
+cp "${old_type}/${old_subtype}/image-detail.md" ${new_markdown_dir}
+cp "${old_type}/${old_subtype}/gallery.md" ${new_markdown_dir}
+sed -i.bak "s/${old_type}/${type}/g" "${new_markdown_dir}/image-detail.md"
+sed -i.bak "s/${old_subtype}/${subtype}/g" "${new_markdown_dir}/image-detail.md"
+sed -i.bak "s/${old_type}/${type}/g" "${new_markdown_dir}/gallery.md"
+sed -i.bak "s/${old_subtype}/${subtype}/g" "${new_markdown_dir}/gallery.md"
+
+echo "Creating template..."
+new_template_dir="_includes/${type}"
+if [ "$is_new_type" = 1 ]
+then
+  mkdir "${new_template_dir}"
+fi
+new_template_file="${new_template_dir}/gallery${subtype}.liquid"
+cp "_includes/${old_type}/gallery${old_subtype}.liquid" "${new_template_file}"
+sed -i.bak "s/${old_type}/${type}/g" ${new_template_file}
+sed -i.bak "s/${old_subtype}/${subtype}/g" ${new_template_file}
