7de56dca497013131a16cfcf8dfb95da874c8bce
Martina Rauert <martinarauert@gmail.com>
2023-01-16 15:03:41 -0600

    st

--- scripts/watermark.bash
+++ scripts/watermark.bash
@@ -2,14 +2,21 @@
 
 dir=$1
 watermark=$2
+out_dir=$3
+
+if [ -z "$out_dir"]
+then
+  out_dir="${dir}"
+fi
 
 for file in $dir*
 do
   echo $file
-  echo "composite $watermark $file -geometry +0+0 $file"
-  composite $watermark $file -geometry +0+0 $file
-  echo "composite $watermark $file -geometry +3000+0 $file"
-  composite $watermark $file -geometry +3000+0 $file
-  echo "composite $watermark $file -geometry +0+3000 $file"
-  composite $watermark $file -geometry +0+3000 $file
+  base_name=$(basename ${file})
+  echo "composite $watermark $file -geometry +0+0 $out_dir$base_name"
+  composite $watermark $file -geometry +0+0 $out_dir$base_name
+  echo "composite $watermark $file -geometry +3000+0 $out_dir$base_name"
+  composite $watermark $file -geometry +3000+0 $out_dir$base_name
+  echo "composite $watermark $file -geometry +0+3000 $out_dir$base_name"
+  composite $watermark $file -geometry +0+3000 $out_dir$base_name
 done
