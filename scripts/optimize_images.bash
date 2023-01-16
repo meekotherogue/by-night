73b58880a105a9e1ea54e9540440cab388203556
Martina Rauert <martinarauert@gmail.com>
2023-01-15 20:37:53 -0600

    TinyPng

--- scripts/optimize_images.bash
+++ scripts/optimize_images.bash
@@ -5,8 +5,8 @@ dir=$1
 for file in $dir*
 do
   echo $file
-  echo "curl --user api:C3CL0yVkS788rnVS25hZT0XW5lhjnVKz --data-binary @$file -i https://api.tinify.com/shrink | jq '.output.url' | tr -d '\"'"
-  url=$(curl --user api:C3CL0yVkS788rnVS25hZT0XW5lhjnVKz --data-binary @$file -s https://api.tinify.com/shrink | jq ".output.url" | tr -d '"')
+  echo "curl --user api:${TINYPNG_API} --data-binary @$file -i https://api.tinify.com/shrink | jq '.output.url' | tr -d '\"'"
+  url=$(curl --user api:${TINYPNG_API} --data-binary @$file -s https://api.tinify.com/shrink | jq ".output.url" | tr -d '"')
   echo "curl $url --output $file"
   downloadResult=$(curl $url --output $file)
 done
