d532b707053c9157fd251a19bdef5eff13999058
Martina Rauert <martinarauert@gmail.com>
2023-01-17 15:01:30 -0600

    Removing debuggers

--- js/event-listeners.js
+++ js/event-listeners.js
@@ -1,15 +1,12 @@
 addEventListener("keydown", (event) => {
   if (event.keyCode == 37) {
     const left = document.getElementById("imageDetailLeftArrow");
-    debugger;
     left.click();
   } else if (event.keyCode == 39) {
     const right = document.getElementById("imageDetailRightArrow");
-    debugger;
     right.click();
   } else if (event.keyCode == 27) {
     const escape = document.getElementById("imageDetailEscape");
-    debugger;
     escape.click();
   }
 });
