35dd09c197cc2ba8da9876362f327eb36dbf7caf
Martina Rauert <martinarauert@gmail.com>
2023-01-17 14:58:17 -0600

    Adding simple JS

--- .eleventy.js
+++ .eleventy.js
@@ -81,6 +81,7 @@ module.exports = function (eleventyConfig) {
 
   // Passthrough Copy
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
   return {
     passthroughFileCopy: true,
   };
